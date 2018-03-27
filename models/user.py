956fd2730497296540a3225e259798984f3f9593
Yunju Chen <info@yunjuchen.net>
2018-03-27 23:05:52 +0000

    Update user

--- models/user.py
+++ models/user.py
@@ -3,13 +3,18 @@
     Implementation of the User class which inherits from BaseModel
 '''
 from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
