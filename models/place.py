c89e83a1e6a1773c3846109a1ace26f80b5a08d6
Yunju Chen <info@yunjuchen.net>
2018-03-28 23:08:48 -0700

    Final update

--- models/place.py
+++ models/place.py
@@ -6,13 +6,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
+from models.amenity import Amenity
 
 
-association_table = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'), nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'), nullable=False))
+place_amenity = Table('place_amenity', Base.metadata,
+                       Column('place_id', String(60),
+                              ForeignKey('places.id'), nullable=False),
+                       Column('amenity_id', String(60),
+                              ForeignKey('amenities.id'), nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -31,10 +33,11 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    amenities = []
 
-    if getenv('DBStorge') == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship('Review', backref='place', cascade='delete')
-        amenities = relastionship('Amenity', secondary='place_amenity',
+        amenities = relationship('Amenity', secondary=place_amenity,
                                   backref='place', viewonly=False)
     else:
         @property
@@ -50,12 +53,16 @@ class Place(BaseModel, Base):
         @property
         def amenities(self):
             '''Getter for amenities, return amenity_ids'''
-            return self.amenity_ids
+            all_amenities = models.storage.all(Amenity)
+            for key, obj in all_amenities.items():
+                if obj.id in self.amenity_ids:
+                    amenities.append(obj)
+            return amenities
 
+        @amenities.setter
         def amenities(self, obj=None):
             '''Setter for amenity_ids'''
-            if obj is not None:
-                if obj.__clasdds__.__name__ == 'Amenity':
-                    self.amenity_ids.append(obj)
-                else:
-                    return
+            if obj.__class__.__name__ == 'Amenity':
+                self.amenity_ids.append(obj.id)
+            else:
+                return
