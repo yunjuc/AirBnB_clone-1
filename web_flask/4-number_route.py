cdfda0e8abff1b50b3d4e36857180a0471d56abe
Yunju <yunju@yunjus-air.attlocal.net>
2018-04-17 11:34:12 -0700

    First commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''Start a flask web application
+'''
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    '''hello hbnb'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''hbnb'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is(text):
+    '''c is...'''
+    text = text.replace('_', ' ')
+    return 'C %s' % text
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text=''):
+    '''python is...'''
+    if text:
+        text = text.replace('_', ' ')
+    else:
+        text = 'is cool'
+    return 'Python %s' % text
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''Display number'''
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
