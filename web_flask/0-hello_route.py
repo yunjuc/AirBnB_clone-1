bb9f323864615360e34690e85701ecc71c148523
Yunju <yunju@yunjus-air.attlocal.net>
2018-04-17 09:36:47 -0700

    Fix pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,14 @@
 '''
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello_hbnb():
     '''First app'''
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
